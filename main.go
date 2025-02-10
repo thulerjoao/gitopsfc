4ad959214978e3f79f1990685c8e550be516163a
thulerjoao <thuler_lima@hotmail.com>
2025-02-10 16:41:49 -0300

    changed to hello with argo

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ import "net/http"
 
 func main () {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello John!!!</h1>"))
+		w.Write([]byte("<h1>Hello John with ARGO!!!</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
